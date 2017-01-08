412b4171589dfb6f7a6683d57ba96db232ad3d79
kmoverall <kmoverall@gmail.com>
2017-01-08 17:45:37 -0500

    Added 3rd person controller standard assets and company libraries

---
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControl : MonoBehaviour {
+
+    [SerializeField]
+    private Transform character;
+    [SerializeField]
+    private Animator characterAnimator;
+
+
+    [SerializeField]
+    private float maxWalkSpeed = 5f;
+    [SerializeField]
+    private float walkAcceleration = 10f;
+    [SerializeField]
+    private float walkDeceleration = 20f;
+    [SerializeField]
+    private float gravityScale = 1f;
+    [SerializeField]
+    private float terminalVelocity = 40f;
+
+    private Vector3 currentMotion;
+
+    void Start () 
+    {
+	    
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
